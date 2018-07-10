4e713bd4e3c679bd6495c8c7ae39ec01e676e2f7
Hiroaki Oshima <kifa0422@gmail.com>
2018-07-09 17:16:24 -0700

    minimum reqs complete

--- mini App1/app.js
+++ mini App1/app.js
@@ -1,9 +1,7 @@
 var turnCounter = 1;
 var symbols = ['O', 'X'];
 var playersStates = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
-var wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-			   [0, 3, 6], [1, 4, 7], [2, 5, 8],
-			   [0, 4, 8], [2, 4, 6]];
+var wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 var isEnd = false;
 
 document.getElementsByClassName('reset')[0].addEventListener("click", function(){
